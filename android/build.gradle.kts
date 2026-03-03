1afa3763e2506649e0b8fe226ee7f4752c6f0912
hitesh-yadav2801 <hiteshyadav3607@gmail.com>
2026-03-03 15:19:17 +0530

    chore: upgrade dependencies and force java 17 compatibility

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -16,6 +16,18 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
+subprojects {
+    afterEvaluate {
+        if (project.extensions.findByName("android") != null) {
+            val android = project.extensions.getByName("android") as com.android.build.gradle.BaseExtension
+            android.compileOptions {
+                sourceCompatibility = JavaVersion.VERSION_17
+                targetCompatibility = JavaVersion.VERSION_17
+            }
+        }
+    }
+}
+
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
